574b01a62f8abc413ff6605edc73efdae9a59ecd
Thales Nascimento <thales.silva.nascimento@gmail.com>
2021-08-07 21:28:13 -0300

    Adicionado o middleware de autenticação

---
+++ src/middleware/ensureAuthenticated.ts
@@ -0,0 +1,41 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+import { AppError } from "../errors/AppError";
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UserRepository";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError("Token missing", 401);
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      "YkJzGjsaa8nw983V268TAC7J9lMso1fLmgl8aKVFmwqVoMdbbVkQWV5X3Q"
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError("User doesn't exist", 401);
+    }
+
+    next();
+  } catch {
+    throw new AppError("Invalid Token", 401);
+  }
+}
